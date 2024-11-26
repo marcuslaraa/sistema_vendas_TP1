9711febf2ba23bc072b6633100f140911742927d
marcuslaraa <marcus.lara@hotmail.com>
2024-11-25 21:32:15 -0300

    feat: created crud Product class

--- sistema_vendas_TP1/Program.cs
+++ sistema_vendas_TP1/Program.cs
@@ -4,7 +4,15 @@ using sistema_vendas_TP1.Repository;
 Client client = new Client("Marcus", 30, "42831413800"); 
 Client client2 = new Client("Adriana", 56, "11111111111");
 
+Product product = new Product("Samsung", "Galaxy S21", "Celular top de linha", 5000.00);
+Product product2 = new Product("Apple", "Iphone 12", "Celular top de linha", 6000.00);
+
+ProductRepository listProducts = new ProductRepository();
+
 ClientRepository listClients = new ClientRepository();
+
+listProducts.addProduct(product);
+listProducts.addProduct(product2);
 
 listClients.addClient(client);
 listClients.addClient(client2);
@@ -12,8 +20,13 @@ listClients.addClient(client2);
 Console.WriteLine(listClients.getAllClients());
 Console.WriteLine(listClients.getClientByCode(client.Code));
 
+Console.WriteLine(listProducts.getAllProducts());
+Console.WriteLine(listProducts.getProductByCode(product.Code));
+
 listClients.deleteClientByCode(client.Code);
+listProducts.deleteProductByCode(product.Code);
 
 Console.WriteLine(listClients.getAllClients());
+Console.WriteLine(listProducts.getAllProducts());
 
 
---
+++ sistema_vendas_TP1/Repository/ProductRepository.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sistema_vendas_TP1.model;
+
+namespace sistema_vendas_TP1.Repository
+{
+    public class ProductRepository
+    {
+        private List<Product> _products = new List<Product>();
+
+        public void addProduct(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public string getAllProducts()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var product in _products)
+            {
+                sb.AppendLine(product.ToString());
+                sb.AppendLine("----------");
+            }
+
+            return sb.ToString();
+        }
+
+        public Product getProductByCode(string code)
+        {
+            Product? product = _products.Find(p => p.Code == code);
+            if (product == null)
+            {
+                throw new Exception($"Produto não encontrado com o código {code}");
+            }
+            return product;
+        }
+
+        public void deleteProductByCode(string code)
+        {
+            Product? product = getProductByCode(code);
+            if (product == null)
+            {
+                throw new Exception($"Produto não encontrado com o código {code}");
+            }
+            _products.Remove(product);
+        }
+    }
+}
